6e8985698def8933d8e1b33ac8aa688439dd2a46
chrisbraunschweiler1 <cbraunschweiler@gmail.com>
2016-01-13 15:41:33 +0100

    Added HttpClient

--- XamiWhammy/NoserTwitterService.cs
+++ XamiWhammy/NoserTwitterService.cs
@@ -1,19 +1,24 @@
 ﻿using System;
+using System.Net;
+using System.Net.Http;
 
 namespace XamiWhammy
 {
 	public class NoserTwitterService : NoserService
 	{
+		HttpClient httpClient;
+
 		public NoserTwitterService ()
 		{
+			httpClient = new HttpClient ();
+			httpClient.MaxResponseContentBufferSize = 256000;
 		}
 
 		public void fetchString(){
-			int ha = 3;
-			if (ha == 3) {
-				int fa = 234;
+			string url = "http://ajax.googleapis.com/ajax/services/search/web?v=1.0&rsz=8&q=";
 
-			}
+			//See: https://developer.xamarin.com/guides/xamarin-forms/web-services/consuming/rest/ for info on how to use HttpClient
+
 		}
 	}
 }
