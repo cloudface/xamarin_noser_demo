f2ea30da94f71b43930e60fc8a20544949904c63
silvan.nellen <silvan.nellen@noser.com>
2016-06-02 14:43:01 +0200

    Load and show tweets

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -6,30 +6,37 @@ using System.Collections.Generic;
 using XamiWhammy.Models;
 using XamiWhammy.Presentation;
 using XamiWhammy.Repos;
+using Android.Views;
 
 namespace XamiWhammy.Droid
 {
 	[Activity (Label = "XamiWhammy", MainLauncher = true, Icon = "@mipmap/icon")]
 	public class MainActivity : Activity, TwitterView
     {
-		int count = 1;
 
         private TwitterPresenter Presenter { get; set; }
 
         private ListView TwitterList { get; set; }
 
+        private TextView NoTweetsLabel { get; set; }
+
+        private ProgressDialog progressDialog { get; set; }
+
         public void ShowNoTweetsMessage()
         {
-            
+            progressDialog.Dismiss();
+            NoTweetsLabel.Visibility = ViewStates.Visible;
+            TwitterList.Visibility = ViewStates.Gone;
         }
 
         public void ShowProgressBar()
         {
-           
+            progressDialog.Show();
         }
 
         public void ShowTweets(List<Tweet> tweets)
         {
+            progressDialog.Dismiss();
             ArrayAdapter adapter = new TweetAdapter(this, tweets);
             TwitterList.Adapter = adapter;
         }
@@ -43,6 +50,11 @@ namespace XamiWhammy.Droid
             Presenter = new TwitterPresenter(this, new TwitterRepoImpl());
 
             TwitterList = FindViewById<ListView> (Resource.Id.twitterList);
+
+            NoTweetsLabel = FindViewById<TextView>(Resource.Id.noTweetsMessage);
+
+            progressDialog = new ProgressDialog(this);
+            progressDialog.SetTitle("Loading Tweets");
 
             Presenter.LoadFeed();
 		}
--- Droid/Resources/Resource.designer.cs
+++ Droid/Resources/Resource.designer.cs
@@ -44,17 +44,20 @@ namespace XamiWhammy.Droid
 		public partial class Id
 		{
 			
-			// aapt resource value: 0x7f050001
-			public const int profilePicture = 2131034113;
-			
-			// aapt resource value: 0x7f050003
-			public const int tweetPreview = 2131034115;
-			
 			// aapt resource value: 0x7f050000
-			public const int twitterList = 2131034112;
+			public const int noTweetsMessage = 2131034112;
 			
 			// aapt resource value: 0x7f050002
-			public const int userName = 2131034114;
+			public const int profilePicture = 2131034114;
+			
+			// aapt resource value: 0x7f050004
+			public const int tweetPreview = 2131034116;
+			
+			// aapt resource value: 0x7f050001
+			public const int twitterList = 2131034113;
+			
+			// aapt resource value: 0x7f050003
+			public const int userName = 2131034115;
 			
 			static Id()
 			{
